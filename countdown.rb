acbff142882091462f30bda072c4f3207464d49d
Terry Threatt <terry.threatt@gmail.com>
2020-02-14 00:22:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(num)
   
   return 'HAPPY NEW YEAR!'
 end
+
+
+def countdown_with_sleep(num)
+  num = 10 
+  
+  while num > 0 
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  
+  return 'HAPPY NEW YEAR!'
+end
